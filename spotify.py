f321f4df508217e89c54ac7b9986c45f5f24efe0
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-05-26 16:21:19 -0400

    Added authentication

--- spotify.py
+++ spotify.py
@@ -1,11 +1,40 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-cid = 'Access Token'
-secret = 'Secret Token'
+def connect_api(filename):
+    filecache = read_client(filename)
+    cid = filecache['client_id']
+    secret = filecache['secret_token']
+    username = filecache['username']
+    return spotipy.util.prompt_for_user_token(username, "playlist-modify-public user-library-read", cid, secret, "http://127.0.0.1:9090")
 
-def connect_api(client_id, client_secret):
-    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+def read_client(filename):
+    filecache = {}
+    with open(filename) as f:
+        for line in f:
+            (key, val) = line.split()
+            filecache[key] = val
+    return filecache
 
-spotify = connect_api(cid, secret)
+# Returns a list of the first x liked songs
+def get_liked_songs(spotify):
+    return spotify.current_user_saved_tracks()
+
+# Compares the liked songs to the json file and adds those which are not present
+def write_liked_songs(songs):
+    #TODO
+    return null
+
+# Analyzes a song based on the K_clustering algorithm and adds it to the specified playlist
+def analyze_song(song):
+    ## TODO
+    return nulll
+
+# Adds the song to the specified playlist
+def add_song(song, playlist):
+    # TODO
+    return null
+
+
+spotify = spotipy.Spotify(connect_api('client_tokens'))
+print(spotify.current_user_saved_tracks(limit=1))
